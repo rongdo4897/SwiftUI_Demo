a20fcfc6e48e48b7930f86b72737a4c4f006cb6f
rongdo4897 <tunglam977@gmail.com>
2024-01-23 19:17:46 +0700

    CoverFlow

---
+++ Collection/CoverFlow/CoverFlow/CoverFlowApp.swift
@@ -0,0 +1,17 @@
+//
+//  CoverFlowApp.swift
+//  CoverFlow
+//
+//  Created by Lam Hoang Tung on 1/23/24.
+//
+
+import SwiftUI
+
+@main
+struct CoverFlowApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
